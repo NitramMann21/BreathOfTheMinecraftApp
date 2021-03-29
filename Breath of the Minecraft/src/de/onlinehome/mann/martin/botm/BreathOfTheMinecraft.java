8f09529279a4384f16dfaf9937a04cf241e40c92
NitramMann21 <martin.mann@onlinehome.de>
2021-03-29 09:53:31 +0200

    Version 1.1 with Update-Message, new Button-Design and YouTube-Tab.

--- Breath of the Minecraft/src/de/onlinehome/mann/martin/botm/BreathOfTheMinecraft.java
+++ Breath of the Minecraft/src/de/onlinehome/mann/martin/botm/BreathOfTheMinecraft.java
@@ -13,22 +13,30 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import com.blogspot.debukkitsblog.net.Datapackage;
 
 public class BreathOfTheMinecraft {
 	
-	JFrame frame;
+	static JFrame frame;
 	JPanel panel;
 	
-	JButton creditsButton;
-	JButton screenshotsButton;
-	JButton newsButton;
-	JButton discordButton;
+	JMenuBar menuBar;
+	JMenu menuTabs;
+	JMenu menuSocialMedia;
+	
+	JMenuItem creditsButton;
+	JMenuItem screenshotsButton;
+	JMenuItem newsButton;
+	JMenuItem discordButton;
+	JMenuItem youtubeButton;
 	
 	JLabel botmIcon;
 	JLabel credits;
@@ -36,8 +44,11 @@ public class BreathOfTheMinecraft {
 	JLabel news;
 	JLabel discord;
 	JLabel discordLink;
+	JLabel youtube;
+	JLabel youtubeLink;
 	
-	String version = "Release 1.0";
+	String version = "v1.1";
+	static int versionId = 4;
 	
 	public static void main(String[] args) {
 		new BreathOfTheMinecraft();
@@ -49,47 +60,73 @@ public class BreathOfTheMinecraft {
 		frame = new JFrame("Breath of the Minecraft");
 		panel = new JPanel();
 		
-		creditsButton = createButton("Credits", 10, 10, 80, 30);
-		screenshotsButton = createButton("Screenshots", 100, 10, 110, 30);
-		newsButton = createButton("News", 220, 10, 100, 30);
-		discordButton = createButton("Discord", 330, 10, 100, 30);
+		menuBar = new JMenuBar();
+		menuTabs = new JMenu("Tabs");
+		menuSocialMedia = new JMenu("Social Media");
+		
+		creditsButton = new JMenuItem("Credits");
+		screenshotsButton = new JMenuItem("Screenshots");
+		newsButton = new JMenuItem("News");
+		discordButton = new JMenuItem("Discord");
+		youtubeButton = new JMenuItem("YouTube");
+		
+		creditsButton.addActionListener(new ButtonClickListener());
+		screenshotsButton.addActionListener(new ButtonClickListener());
+		newsButton.addActionListener(new ButtonClickListener());
+		discordButton.addActionListener(new ButtonClickListener());
+		youtubeButton.addActionListener(new ButtonClickListener());
 		
 		botmIcon = new JLabel(icon);
 		credits = new JLabel("<html><body>Die Breath of the Minecraft-App wurde von Nitram21 programmiert.<br>Version: " + version + "</body></html>");
 		screenshots = new JLabel();
 		news = new JLabel();
-		discord = new JLabel("<html><body>Du willst immer auf dem laufenden bleiben? Dann tritt doch unserem Discord-Server bei,<br>indem du </body></html>");
+		discord = new JLabel("<html><body>Du willst immer auf dem laufenden bleiben? Dann tritt doch unserem Discord-Server bei,<br>indem du</body></html>");
 		discordLink = new JLabel("hier klickst!");
+		youtube = new JLabel("<html><body>Du willst immer auf dem laufenden bleiben? Dann schau doch bei unserem YouTube-Kanal vorbei,<br>indem du </body></html>");
+		youtubeLink = new JLabel("hier klickst!");
 		
 		credits.setForeground(Color.WHITE);
 		screenshots.setForeground(Color.WHITE);
 		news.setForeground(Color.WHITE);
 		discord.setForeground(Color.WHITE);
 		discordLink.setForeground(Color.BLUE);
+		youtube.setForeground(Color.WHITE);
+		youtubeLink.setForeground(Color.BLUE);
 
+		menuTabs.add(creditsButton);
+		menuTabs.add(screenshotsButton);
+		menuTabs.add(newsButton);
+		menuTabs.addSeparator();
+		menuTabs.add(menuSocialMedia);
+		menuSocialMedia.add(discordButton);
+		menuSocialMedia.add(youtubeButton);
+		
+		menuBar.add(menuTabs);
+		
 		frame.add(botmIcon);
-		frame.add(creditsButton);
 		frame.add(credits);
-		frame.add(screenshotsButton);
 		frame.add(screenshots);
-		frame.add(newsButton);
 		frame.add(news);
-		frame.add(discordButton);
 		frame.add(discord);
 		frame.add(discordLink);
+		frame.add(youtube);
+		frame.add(youtubeLink);
 		frame.add(panel);
 
 		frame.setBounds(10, 10, 1200, 650);
 		
-		botmIcon.setBounds(10, 45, 64, 64);
-		credits.setBounds(70, 40, 600, 80);
+		botmIcon.setBounds(10, 10, 64, 64);
+		credits.setBounds(70, 10, 600, 80);
 		screenshots.setBounds(frame.getWidth() / 2 - 684 / 2, frame.getHeight() / 2 - 354 / 2, 684, 354);
-		news.setBounds(70, 40, 600, 100);
-		discord.setBounds(70, 40, 600, 80);
-		discordLink.setBounds(126, 73, 100, 30);
+		news.setBounds(70, 10, 600, 100);
+		discord.setBounds(70, 10, 600, 80);
+		discordLink.setBounds(126, 43, 100, 30);
+		youtube.setBounds(70, 10, 600, 80);
+		youtubeLink.setBounds(126, 43, 100, 30);
 		
 		frame.setIconImage(icon.getImage());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setJMenuBar(menuBar);
 		
 		panel.setOpaque(true);
 		panel.setBackground(Color.DARK_GRAY);
@@ -100,11 +137,16 @@ public class BreathOfTheMinecraft {
 		news.setVisible(false);
 		discord.setVisible(false);
 		discordLink.setVisible(false);
+		youtube.setVisible(false);
+		youtubeLink.setVisible(false);
+		menuBar.setVisible(true);
+		menuTabs.setVisible(true);
 		
 		frame.setVisible(true);
 		
+		getVersion();
+		
 		discordLink.addMouseListener(new MouseAdapter() {
-			
 			@Override
 			public void mouseClicked(MouseEvent event) {
 				try {
@@ -113,18 +155,28 @@ public class BreathOfTheMinecraft {
 					e.printStackTrace();
 				}
 			}
-			
+		});
+		youtubeLink.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent event) {
+				try {
+					Desktop.getDesktop().browse(new URI("https://www.youtube.com/channel/UC4PG2TobTvmsOVTWFClnmWA"));
+				} catch (IOException | URISyntaxException e) {
+					e.printStackTrace();
+				}
+			}
 		});
 	}
 	
-	public JButton createButton(String text, int x, int y, int width, int height) {
-		JButton button = new JButton(text);
-		button.setBounds(x, y, width, height);
-		button.addActionListener(new ButtonClickListener());
-		button.setOpaque(true);
-		button.setBackground(Color.GRAY);
-		button.setVisible(true);
-		return button;
+	public static void getVersion() {
+		BotMClient client = new BotMClient("botmserver.ddnss.de", 7921);
+		client.setMuted(true);
+		Datapackage result = client.sendMessage(new Datapackage("GET_NEWEST_VERSION"));
+		int newestVersionId = (int) result.get(1);
+		
+		if(newestVersionId > versionId) {
+			JOptionPane.showMessageDialog(frame, "Eine neue Version ist verfügbar!");
+		}
 	}
 	
 	public class ButtonClickListener implements ActionListener {
@@ -137,20 +189,21 @@ public class BreathOfTheMinecraft {
 				news.setVisible(false);
 				discord.setVisible(false);
 				discordLink.setVisible(false);
+				youtube.setVisible(false);
+				youtubeLink.setVisible(false);
 			} else if(e.getSource() == screenshotsButton) {
 				credits.setVisible(false);
 				screenshots.setVisible(true);
 				news.setVisible(false);
 				discord.setVisible(false);
 				discordLink.setVisible(false);
+				youtube.setVisible(false);
+				youtubeLink.setVisible(false);
 				
 				BotMClient client = new BotMClient("botmserver.ddnss.de", 7921);
-				Datapackage result = client.sendMessage(new Datapackage("FETCH_SCREENSHOTS"));
-				
-				if(result.get(0).equals("ERROR")) {
-					screenshots.setIcon(null);
-					screenshots.setText("An error occured whilst trying to fetch current screenshots.");
-				} else {
+				client.setMuted(true);
+				if(client.isServerReachable()) {
+					Datapackage result = client.sendMessage(new Datapackage("FETCH_SCREENSHOTS"));
 					String fileName = "BreathOfTheMinecraftAppScreenshots.jpg";
 					
 					File file = new File(fileName);
@@ -167,6 +220,9 @@ public class BreathOfTheMinecraft {
 					
 					screenshots.setIcon(new ImageIcon("BreathOfTheMinecraftAppScreenshots.jpg"));
 					screenshots.setText(null);
+				} else {
+					screenshots.setIcon(null);
+					screenshots.setText("An error occured whilst trying to fetch current screenshots.");
 				}
 			} else if(e.getSource() == newsButton) {
 				credits.setVisible(false);
@@ -174,17 +230,20 @@ public class BreathOfTheMinecraft {
 				news.setVisible(true);
 				discord.setVisible(false);
 				discordLink.setVisible(false);
+				youtube.setVisible(false);
+				youtubeLink.setVisible(false);
 				
 				BotMClient client = new BotMClient("botmserver.ddnss.de", 7921);
-				Datapackage result = client.sendMessage(new Datapackage("FETCH_NEWS"));
-				
-				if(result.get(0).equals("ERROR")) {
-					news.setText("An error occured whilst trying to fetch current news.");
-				} else {
+				client.setMuted(true);
+				if(client.isServerReachable()) {
+					Datapackage result = client.sendMessage(new Datapackage("FETCH_NEWS"));
+					
 					Datapackage newsDatapackage = (Datapackage) result.get(1);
 					String newsText = (String) newsDatapackage.get(1);
 					
 					news.setText(newsText);
+				} else {
+					news.setText("An error occured whilst trying to fetch current news.");
 				}
 			} else if(e.getSource() == discordButton) {
 				credits.setVisible(false);
@@ -192,6 +251,16 @@ public class BreathOfTheMinecraft {
 				news.setVisible(false);
 				discord.setVisible(true);
 				discordLink.setVisible(true);
+				youtube.setVisible(false);
+				youtubeLink.setVisible(false);
+			} else if(e.getSource() == youtubeButton) {
+				credits.setVisible(false);
+				screenshots.setVisible(false);
+				news.setVisible(false);
+				discord.setVisible(false);
+				discordLink.setVisible(false);
+				youtube.setVisible(true);
+				youtubeLink.setVisible(true);
 			}
 		}
 		
